4d5558726c5d8ad626e1e4cc8b4d79f5c2d410f3
Edgar <15989069487@163.com>
2020-08-16 23:19:09 +0800

    feat: 通用CRUD
    如/context/rest/:resource的接口，自动生成CRUD接口，模型名称为resource转后的类名

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,33 +1,48 @@
 module.exports = (app) => {
   const express = require("express");
-  const router = express.Router();
-  const Category = require("../../models/Category");
-  router.post("/categories", async (req, res) => {
-    const model = await Category.create(req.body);
+  const router = express.Router({
+    mergeParams: true, //
+  });
+  router.post("/", async (req, res) => {
+    const model = await req.Model.create(req.body);
     res.send(model);
   });
 
-  router.put("/categories/:id", async (req, res) => {
-    const model = await Category.findByIdAndUpdate(req.params.id, req.body);
+  router.put("/:id", async (req, res) => {
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
     res.send(model);
   });
 
-  router.delete("/categories/:id", async (req, res) => {
-    const model = await Category.findByIdAndDelete(req.params.id);
+  router.delete("/:id", async (req, res) => {
+    await req.Model.findByIdAndDelete(req.params.id);
     res.send({
       success: true,
     });
   });
 
-  router.get("/categories", async (req, res) => {
-    const model = await Category.find().populate('parent');
+  router.get("/", async (req, res) => {
+    const queryOption = {}
+    if (req.Model.modelName === 'Category') {
+      queryOption.populate = 'parent'
+    }
+    const model = await req.Model.find().setOptions(queryOption);
     res.send(model);
   });
 
-  router.get("/categories/:id", async (req, res) => {
-    const model = await Category.findById(req.params.id);
+  router.get("/:id", async (req, res) => {
+    const model = await req.Model.findById(req.params.id);
     res.send(model);
   });
 
-  app.use("/admin/api", router);
+
+  // 中间件中的动态路由参数，不能再router中直接获取，需要在实例化router时，传入mergeParams=true
+  app.use("/admin/api/rest/:resource", (req, res, next) => {
+    const inflection = require('inflection')
+    const modelName = inflection.classify(req.params.resource)
+    
+    const Model = require(`../../models/${modelName}`);
+    req.Model = Model
+    next()
+
+  }, router);
 };
