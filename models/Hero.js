0bf5dab5c385f14d9c52aaad916c3ccdfa292035
Edgar <15989069487@163.com>
2020-08-23 19:34:07 +0800

    feat: 英雄技能管理模型

--- models/Hero.js
+++ models/Hero.js
@@ -1,12 +1,78 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: {
-        type: String,
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  categories: [{
+    type: mongoose.Types.ObjectId,
+    ref: "Category",
+  }],
+  properties: {
+    difficulty: {
+      type: Number,
     },
-    avatar: {
-        type: String,
+    skill: {
+        type: Number
+    },
+    attack: {
+        type: Number
+    },
+    servive: {
+        type: Number
     }
-})
+  },
 
-module.exports = mongoose.model('Hero', schema)
+  // 技能介绍
+  skills: [
+      {
+        icon: String,
+        name: String,
+        description: String,
+        consume: Number, // 消耗
+        restore: String, // 冷却值, 存在多个
+      }
+  ],
+
+  // 加点建议
+//  mainSkill: {
+//      type: mongoose.Types.ObjectId,
+//      ref: ''
+//  }
+// 顺风出装
+attackEquipment: [
+    {
+        type: mongoose.Types.ObjectId,
+        ref: 'Item'
+    }
+],
+// 逆风出装
+serviveEquipment: [
+    {
+        type: mongoose.Types.ObjectId,
+        ref: 'Item'
+    }   
+],
+// 使用技巧
+usageTip: {
+    type: String
+},
+// 对战技巧
+battleTip: {
+    type: String
+},
+// 团战思路
+teamFightTip: {
+    type: String
+},
+// 英雄关系
+
+});
+
+module.exports = mongoose.model("Hero", schema);
