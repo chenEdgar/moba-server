a91593ca4d881fb9cca52309b433862b4be8f6b0
Edgar <15989069487@163.com>
2020-08-25 08:52:13 +0800

    feat: 增加广告位模型

---
+++ models/Ad.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  items: [
+    {
+      image: String,
+      url: String,
+    },
+  ],
+});
+
+module.exports = mongoose.model("Ads", schema);
